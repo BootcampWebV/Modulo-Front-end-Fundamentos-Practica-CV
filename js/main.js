c2dc2319baac2d2c94c29fe7b4540a64965c251f
Angelvilad <angelvilad@hotmail.com>
2018-07-16 19:44:44 +0200

    implemento smooth scroll

--- js/main.js
+++ js/main.js
@@ -8,9 +8,35 @@ export class Main {
     }
 
     defineEventListeners() {
+        
+        this.prepararNavegacion();
         window.addEventListener('scroll', this.offsetChangeStyle.bind(this));
 
-        this.prepararNavegacion();
+       document.querySelector('.link-to-h').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('home');
+        });
+        document.querySelector('.link-to-wai').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('who-am-i');
+        });
+        document.querySelector('.link-to-ab').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('academic-background');
+        });
+        document.querySelector('.link-to-je').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('job-experience');
+        });
+        document.querySelector('.link-to-am').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('about-me');
+        });
+        document.querySelector('.link-to-c').addEventListener('click', (e) => {
+            e.preventDefault();
+            this.smoothScroll('contact');
+        });
+        
     }
 
     offsetChangeStyle() {
@@ -24,8 +50,10 @@ export class Main {
             menuItem = 2;
         } else if (pageOffset >= this.oOffsets['#job-experience'] && pageOffset < this.oOffsets['#about-me']) {
             menuItem = 3;
-        } else {
+        } else if (pageOffset >= this.oOffsets['#about-me'] && pageOffset < this.oOffsets['#contact']) {
             menuItem = 4;
+        } else {
+            menuItem = 5;
         }
 
         this.aMenu.forEach(
@@ -39,6 +67,7 @@ export class Main {
             (item) => {
                 let cumulative =  this.cumulativeOffset(item);
                 this.oOffsets['#'+item.id] = cumulative;
+                console.log(cumulative);
             }
         )
     }
@@ -51,4 +80,51 @@ export class Main {
         } while(element);
         return top;
     }
+
+    currentYPosition() {
+        // Firefox, Chrome, Opera, Safari
+        if (self.pageYOffset) return self.pageYOffset;
+        // Internet Explorer 6 - standards mode
+        if (document.documentElement && document.documentElement.scrollTop)
+            return document.documentElement.scrollTop;
+        // Internet Explorer 6, 7 and 8
+        if (document.body.scrollTop) return document.body.scrollTop;
+        return 0;
+    }
+
+    elmYPosition(eID) {
+        var elm = document.getElementById(eID);
+        var y = elm.offsetTop;
+        var node = elm;
+        while (node.offsetParent && node.offsetParent != document.body) {
+            node = node.offsetParent;
+            y += node.offsetTop;
+        } return y;
+    }
+
+    smoothScroll(eID) {
+        var startY = this.currentYPosition();
+        var stopY = this.elmYPosition(eID);
+        var distance = stopY > startY ? stopY - startY : startY - stopY;
+        if (distance < 100) {
+            scrollTo(0, stopY); return;
+        }
+        var speed = Math.round(distance / 100);
+        if (speed >= 20) speed = 20;
+        var step = Math.round(distance / 25);
+        var leapY = stopY > startY ? startY + step : startY - step;
+        var timer = 0;
+        if (stopY > startY) {
+            for ( var i=startY; i<stopY; i+=step ) {
+                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+            } return;
+        }
+        for ( var i=startY; i>stopY; i-=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        }
+    }
+
+
 }
